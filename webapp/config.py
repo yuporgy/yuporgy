88f19866d2d9076e50510afe9a02fba48275d7c5
Max Botov <yuporgy@gmail.com>
2022-07-16 16:42:45 +0800

    Вторая версия, без модальных окон

--- webapp/config.py
+++ webapp/config.py
@@ -2,8 +2,8 @@ from datetime import timedelta
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
 SECRET_KEY = "C0dGtV{c5cZ?u?er6%Gn"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 REMEBER_COOCKIE_DURATION = timedelta(days=5)
--- webapp/people/views.py
+++ webapp/people/views.py
@@ -1,6 +1,8 @@
 
+from turtle import update
 from flask import Blueprint, render_template, current_app, redirect, request, url_for
 from flask_login import current_user
+from webapp import people
 from webapp.people.forms import AddPeople
 from webapp.people.models import People
 from datetime import datetime, date
@@ -13,7 +15,6 @@ def index():
     if not current_user.is_authenticated:
         return redirect(url_for('user.login'))
     page_title="Список дней рождений"
-    form=AddPeople()
     peoples = People.query.filter(People.user_id == current_user.id).all()
     for people in peoples:
         if date.today() > people.date_of_birth.replace(year=datetime.now().year):
@@ -24,10 +25,20 @@ def index():
             people.age = datetime.now().year - people.date_of_birth.year - 1
         
     peoples = sorted(peoples, key=attrgetter('day_to_bd'),reverse=False)
-    return render_template('people/index.html', peoples=peoples, page_title=page_title,form=form)
+    return render_template('people/index.html', peoples=peoples, page_title=page_title)
 
-@blueprint.route('/add', methods = ['POST'])
+@blueprint.route('/view/<id>', methods = ['GET','POST'])
+def view(id):
+    if request.method == 'GET':
+        data = People.query.get(id)
+        return render_template('people/view.html',people=data)
+
+
+@blueprint.route('/create', methods = ['GET','POST'])
 def add():
+    form=AddPeople()
+    if request.method == 'GET':
+        return render_template('people/create.html',form=form)
     if request.method == 'POST' :
         user_id = current_user.id
         name = request.form['name']
@@ -45,3 +56,36 @@ def add():
 
         return redirect(url_for('people.index'))
 
+@blueprint.route('/update/<id>', methods = ['GET','POST'])
+def update(id):
+    if request.method == 'GET':
+        print(id)
+        data = People.query.filter_by(id=id).first()
+        print(data)
+        return render_template('people/update.html', people=data)
+    if request.method == 'POST' :
+        data = People.query.get(request.form.get('id'))
+        print(data)
+        data.name = request.form['name']
+        data.group_id = request.form['group_id']
+        date_of_birth = request.form['date_of_birth']
+        data.date_of_birth = datetime.strptime(date_of_birth, '%Y-%m-%d')
+        data.phone = request.form['phone']
+        data.profile = request.form['profile']
+        data.hobbies = request.form['hobbies']
+        data.reminder_rule = request.form['reminder_rule']
+
+        db.session.commit()
+
+        return redirect(url_for('people.index'))
+
+@blueprint.route('/delete/<id>/', methods = ['GET','POST'])
+def delete(id):
+        if request.method == 'GET':
+            data = People.query.filter_by(id=id).first()
+            return render_template('people/delete.html', people=data)
+        if request.method == 'POST' :
+            data = People.query.get(id)
+            db.session.delete(data)
+            db.session.commit()
+            return redirect(url_for('people.index'))
